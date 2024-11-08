f5edb41829af14db0c5416902187f4740d64b46d
atripathi6 <atripathi6@dxc.com>
2024-11-08 18:42:48 +0530

    first commit from VS code

---
+++ Main.js
@@ -0,0 +1,48 @@
+// let a = true;
+// let b = true;
+// console.log(a && b);
+
+
+//template leteral
+
+// let name1 = "ashish";
+// let name2 = "tripathi";
+
+// console.log(name1 + " " + name2, '${name1}');
+
+// Ternary Operators
+
+/* let isrecepieready = false;
+
+function GetRecipeName(name) {
+    return name;
+
+}
+
+if (isrecepieready) {
+    console.log("Indian food")
+
+}
+else {
+    console.log("Chiness food")
+}
+
+isrecepieready ?
+    console.log(GetRecipeName("India Food"))
+    : console.log(GetRecipeName("Chiness Food")); */
+
+
+let studentArray = [1, 2, 3]
+
+//let getfirstarray = studentArray[0];
+// let getSecondarray = studentArray[1];
+//let getThirdarray = studentArray[2];
+
+//console.log(getfirstarray, getSecondarray, getThirdarray);
+
+
+const [first, secod] = studentArray;
+console.log(first, secod);
+
+
+
